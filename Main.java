52bf86964acdba3bb2f050dec5ab535538e990ed
annatasio <147800087+annatasio@users.noreply.github.com>
2023-10-20 10:35:27 +0300

    Add files via upload
    The begging of our Assingment

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("Assignment for Programming II, Department of Management Science and Technology");
+		System.out.println();
+		System.out.println("Professor: Dr Spinellis Diomidis");
+		System.out.println();
+		System.out.println("Leader of the team:");
+		System.out.println("Anna Tasiopoulou");
+		System.out.println();
+		System.out.println("Members of the tea:");
+	}
+}
