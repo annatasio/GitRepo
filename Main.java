e6fa662dffb987f94e932848741e58c063bf2fee
Anna Tasiopoulou <t8220147@aueb.gr>
2023-11-11 18:21:08 +0200

    An example for monitor thread handling metrics

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("Assignment for Programming II, Department of Management Science and Technology");
+		System.out.println();
+		System.out.println("Professor: Dr Spinellis Diomidis");
+		System.out.println();
+		System.out.println("Leader of the team:");
+		System.out.println("Anna Tasiopoulou, 8220147");
+		System.out.println();
+		System.out.println("Members of the team: ");
+		System.out.println();
+		System.out.println("Georgia Papagianni, 8220115");
+		System.out.println("Ioannis Varelis, 8220009");
+		System.out.println("Alexis Barklesias, 8190304");
+		System.out.println("Konstantinos Lekkas, 8220077");
+	}
+}
