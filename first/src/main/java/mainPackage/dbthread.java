a180a12d6bcd3ba27679f2f914c015cea016556f
Ioannis Varelis <t8220009@aueb.gr>
2023-12-03 19:24:54 +0200

    Established JDBC Connection to SQL Server

---
+++ first/src/main/java/mainPackage/dbthread.java
@@ -0,0 +1,16 @@
+package mainPackage;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class dbthread 
+{
+    public static void main(String[] args) {
+        String connectionUrl = "jdbc:sqlserver://localhost:1433;databaseName=Demo;user=testLogin;password=1234;" +
+        "encrypt=true;trustServerCertificate=true;";
+        try (Connection connection = DriverManager.getConnection(connectionUrl)) {
+            System.out.println("Connected to SQL Server!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+} 
