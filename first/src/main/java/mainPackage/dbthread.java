a817af947f82be758102774f7a3222cee73af31b
Ioannis Varelis <t8220009@aueb.gr>
2023-12-07 01:17:58 +0200

    Update name of the class

--- first/src/main/java/mainPackage/dbthread.java
+++ first/src/main/java/mainPackage/dbthread.java
@@ -1,9 +1,10 @@
 package mainPackage;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-public class dbthread 
-{
+
+public class DBThread {
     public static void main(String[] args) {
         String connectionUrl = "jdbc:sqlserver://localhost:1433;databaseName=Demo;user=testLogin;password=1234;" +
         "encrypt=true;trustServerCertificate=true;";
